0cab9aba3651ecf57f4939c7168d086ec5eb2a28
Daniel Cender <danielcender@gmail.com>
2020-09-19 01:15:43 -0500

    :fire: 3 of the 4 wk3 assignments

---
+++ Wk3/GreatestCommonDivisor/GreatestCommonDiviser/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace GreatestCommonDivisor
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int number1 = 800;
+            int number2 = 85;
+            int answer = gcd(number1, number2);
+            Console.Out.WriteLine("The gcd of {0} and {1} is {2}", number1, number2, answer);
+            Console.ReadLine();
+        }
+
+        public static int gcd(int n1, int n2)
+        {
+            if(n2 == 0)
+            {
+                return n1;
+            } else
+            {
+                Console.Out.WriteLine("Not yet. Remainder is {0}", n1 % n2);
+                return gcd(n2, n1 % n2);
+            }
+        }
+    }
+}
