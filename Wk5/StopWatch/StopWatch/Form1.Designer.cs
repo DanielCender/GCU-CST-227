65df3b35a0be2b8cc4a43397f8596eeb863ad03c
Daniel Cender <danielcender@gmail.com>
2020-10-02 21:42:29 -0500

    acitivty 5 almost finished

--- Wk5/StopWatch/StopWatch/Form1.Designer.cs
+++ Wk5/StopWatch/StopWatch/Form1.Designer.cs
@@ -29,17 +29,22 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.button4 = new System.Windows.Forms.Button();
+            this.nbrHitsLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nbrFalseHitsLabel = new System.Windows.Forms.Label();
+            this.playAgainBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(53, 504);
+            this.button1.Location = new System.Drawing.Point(31, 504);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(115, 47);
             this.button1.TabIndex = 0;
@@ -58,7 +63,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(193, 504);
+            this.button2.Location = new System.Drawing.Point(163, 504);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(115, 47);
             this.button2.TabIndex = 4;
@@ -68,7 +73,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(337, 504);
+            this.button3.Location = new System.Drawing.Point(297, 504);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(115, 47);
             this.button3.TabIndex = 5;
@@ -84,19 +89,65 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(297, 235);
+            this.button4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button4.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
+            this.button4.Location = new System.Drawing.Point(193, 193);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(141, 41);
+            this.button4.Size = new System.Drawing.Size(359, 163);
             this.button4.TabIndex = 6;
             this.button4.Text = "TARGET";
-            this.button4.UseVisualStyleBackColor = true;
+            this.button4.UseVisualStyleBackColor = false;
             this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // nbrHitsLabel
+            // 
+            this.nbrHitsLabel.AutoSize = true;
+            this.nbrHitsLabel.Location = new System.Drawing.Point(175, 21);
+            this.nbrHitsLabel.Name = "nbrHitsLabel";
+            this.nbrHitsLabel.Size = new System.Drawing.Size(133, 25);
+            this.nbrHitsLabel.TabIndex = 7;
+            this.nbrHitsLabel.Text = "nbrHitsLabel";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(467, 515);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 25);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Game State:";
+            // 
+            // nbrFalseHitsLabel
+            // 
+            this.nbrFalseHitsLabel.AutoSize = true;
+            this.nbrFalseHitsLabel.Location = new System.Drawing.Point(382, 21);
+            this.nbrFalseHitsLabel.Name = "nbrFalseHitsLabel";
+            this.nbrFalseHitsLabel.Size = new System.Drawing.Size(133, 25);
+            this.nbrFalseHitsLabel.TabIndex = 9;
+            this.nbrFalseHitsLabel.Text = "nbrFalseHits";
+            // 
+            // playAgainBtn
+            // 
+            this.playAgainBtn.Location = new System.Drawing.Point(632, 12);
+            this.playAgainBtn.Name = "playAgainBtn";
+            this.playAgainBtn.Size = new System.Drawing.Size(207, 47);
+            this.playAgainBtn.TabIndex = 10;
+            this.playAgainBtn.Text = "Play Again";
+            this.playAgainBtn.UseVisualStyleBackColor = true;
+            this.playAgainBtn.Click += new System.EventHandler(this.playAgainBtn_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(752, 578);
+            this.ClientSize = new System.Drawing.Size(978, 578);
+            this.Controls.Add(this.playAgainBtn);
+            this.Controls.Add(this.nbrFalseHitsLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.nbrHitsLabel);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -105,6 +156,7 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -118,6 +170,10 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label nbrHitsLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label nbrFalseHitsLabel;
+        private System.Windows.Forms.Button playAgainBtn;
     }
 }
 
--- Wk5/StopWatch/StopWatch/Form1.cs
+++ Wk5/StopWatch/StopWatch/Form1.cs
@@ -15,9 +15,27 @@ namespace StopWatch
     {
         public static Stopwatch watch = new Stopwatch();
         public static Random rand = new Random();
+        public static int nbrHits = 0;
+        public static int formClicks = 0;
         public Form1()
         {
             InitializeComponent();
+            nbrHitsLabel.Text = String.Format("Hits: {0}", nbrHits);
+            nbrFalseHitsLabel.Text = String.Format("False Hits: {0}", formClicks);
+            playAgainBtn.Visible = false;
+        }
+
+        public void disableButtons()
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+        }
+        public void enableButtons()
+        {
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -50,15 +68,55 @@ namespace StopWatch
                 ts.Milliseconds / 10);
 
             // Move the button to a random place on the form
-            if(button4.Visible)
-                button4.Location = new Point(rand.Next(60, 660), rand.Next(30, 490));
+           //if(button4.Visible)
+           // Originally, button was only clickable once.
+           //   Now, it can be clicked until a game-ending state is arrived
+           button4.Location = new Point(rand.Next(60, 660), rand.Next(30, 490));
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            button4.Visible = false;
+            // button4.Visible = false;
+            nbrHits += 1;
+            nbrHitsLabel.Text = String.Format("Hits: {0}", nbrHits);
+            if (nbrHits >= 5)
+            {
+                watch.Stop();
+                button4.Visible = false;
+                label2.Text += " Game Won!";
+                disableButtons();
+                playAgainBtn.Visible = true;
+            }
             // Stop watch to record time taken to click the "mole" button
-            watch.Stop();
+            //watch.Stop();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            // Add to illegal form clicks, counts towards game ending
+            formClicks += 1;
+            nbrFalseHitsLabel.Text = string.Format("False Hits: {0}", formClicks);
+            if (formClicks >= 3)
+            {
+                watch.Stop();
+                button4.Visible = false;
+                // Set game state to "defeat"
+                label2.Text += " Game Over";
+                disableButtons();
+                playAgainBtn.Visible = true;
+            }
+        }
+
+        private void playAgainBtn_Click(object sender, EventArgs e)
+        {
+            formClicks = 0;
+            nbrFalseHitsLabel.Text = string.Format("False Hits: {0}", formClicks);
+            nbrHits = 0;
+            nbrHitsLabel.Text = String.Format("Hits: {0}", nbrHits);
+            label2.Text += "";
+            enableButtons();
+            playAgainBtn.Visible = true;
+            watch.Start();
         }
     }
 }
